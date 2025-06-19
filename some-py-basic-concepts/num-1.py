82169655a408e3f034d2b3ca4130cc2a34a0b573
J.Mohammadi <j.mohammadi@partocrs.com>
2025-06-19 22:11:39 +0330

    Initial commit with project setup and basic structure.

---
+++ some-py-basic-concepts/num-1.py
@@ -0,0 +1,111 @@
+# #type casting ✅✅✅
+# a='1'
+# b=2
+# print(int(a)+b) #3
+
+# #string indexing ✅✅✅
+name='harry'
+# print(name[0]) #h
+
+# #string slicing ✅✅✅
+# print(name[0:3]) #har
+# print(name[0:]) #harry
+# print(name[0:4:2]) #hr  #step argument
+# print(name[::-1]) #yrrah  #reverse string
+
+#string methods ✅✅✅
+# website='http://google.com'
+# print(website.replace('http://','https://')) #https://google.com
+# slice=slice(8,-4)
+# print(website[slice]) #google
+
+# #string methods ✅✅✅
+# name='harry'
+# print(name.upper()) #HARRY
+# print(name.lower()) #harry
+# print(name.title()) #Harry
+# print(name.count('h')) #2
+
+# #string formating ✅✅✅
+# name='harry'
+# print(f'hello {name}') #hello harry
+
+
+#math module ✅✅✅
+# import math
+# print(math.sqrt(16)) #4
+# print(math.pow(2,3)) #8
+# print(math.pi) #3.141592653589793
+# print(math.e) #2.718281828459045
+# print(math.ceil(1.2)) #2
+# print(math.floor(1.2)) #1
+# print(math.factorial(3)) #6
+#print(dir(math))#['__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'ceil', 'copysign', 'cos', 'cosh', 'degrees', 'e', 'erf', 'erfc', 'exp', 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot', 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'ldexp', 'lgamma', 'log', 'log10', 'log1p', 'log2', 'modf', 'nan', 'pi', 'pow', 'radians', 'sin', 'sinh', 'sqrt', 'tan', 'tanh', 'tau', 'trunc']
+
+
+
+#built-in function in Python.✅✅✅
+#print(round(1.2)) #1
+#print(abs(-1)) #1
+#print(len('hello')) #5
+#print(len((1,2,3,4,5))) #5
+#print(len({'a':1,'b':2,'c':3})) #3
+#print(len({'a','b','c'})) #3
+# print(max(1,2,3,4,5)) #5
+#print(min(1,2,3,4,5)) #1
+# print(pow(2,3)) #8
+#print(sqrt(16)) #4
+#print(sin(1)) #0.8414709848078965
+#print(cos(1)) #0.5403023058681398
+#print(tan(1)) #1.5574077246549023
+
+
+#if else statement ✅✅✅
+# age=int(input('enter your age: '))
+# if age>=18:
+#     print('you are an adult')
+# elif age>=13:
+#     print('you are a teenager')
+# else:
+#     print('you are a child')
+
+#logical operators ✅✅✅
+# and
+# or
+# not
+
+#and operator ✅✅✅
+# print(True and True) #True
+# print(True and False) #False
+# print(False and True) #False
+# print(False and False) #False
+#or operator ✅✅✅
+# print(True or True) #True
+# print(True or False) #True
+# print(False or True) #True
+# print(False or False) #False
+#not operator ✅✅✅
+# print(not True) #False
+# print(not False) #True
+
+#while loop ✅✅✅
+# i=0
+# while i<10:
+#     print(i)
+#     i+=1
+
+#for loop ✅✅✅
+# for i in range(10):
+#     print(i)
+
+#break statement ✅✅✅
+# for i in range(10):
+#     print(i)
+#     if i==5:
+#         break
+
+#nested loop ✅✅✅
+# for i in range(10):
+#     for j in range(10):
+#         print(i,j)
+
